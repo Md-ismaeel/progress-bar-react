6f580ff8c8a8fa90d6f9634a26bdbf737d4e28f4
Md Ismail <mdismaeelkhan345@gmail.com>
2024-03-14 13:32:54 +0530

    Initila commit

---
+++ src/Components/ProgressBarHome.js
@@ -0,0 +1,9 @@
+import { ProgressBarCart } from "./ProgressBarCart";
+
+export const ProgressBarHome = () => {
+    return (
+        <>
+            <ProgressBarCart width="45%" height="2rem" duration='200' />
+        </>
+    );
+}
